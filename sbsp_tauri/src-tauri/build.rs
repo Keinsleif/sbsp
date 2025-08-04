1343f49b2eb38f520a663fa9a0500fcabfae7759
Keinsleif <mountaindull@gmail.com>
2025-08-04 23:30:06 +0900

    [update] build.rsにmodelを変換する処理を追加

--- sbsp_tauri/src-tauri/build.rs
+++ sbsp_tauri/src-tauri/build.rs
@@ -1,3 +1,11 @@
+use sbsp_backend::model::ShowModel;
+use ts_rs::TS;
+
 fn main() {
+    println!("cargo:rerun-if-changed=../../sbsp_backend/src/model.rs");
+    println!("cargo:rerun-if-changed=../../sbsp_backend/src/model/");
+
+    <ShowModel as TS>::export_all_to("../src/types/").unwrap();
+
     tauri_build::build()
 }
