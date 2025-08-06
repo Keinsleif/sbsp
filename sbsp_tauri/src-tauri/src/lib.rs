a3e61b7bfe28ae592a1fb731b2810aa99c9a250b
Keinsleif <mountaindull@gmail.com>
2025-08-06 10:57:19 +0900

    [add] set_playback_cursor command

--- sbsp_tauri/src-tauri/src/lib.rs
+++ sbsp_tauri/src-tauri/src/lib.rs
@@ -1,6 +1,9 @@
+use std::str::FromStr;
+
 use sbsp_backend::{controller::{ControllerCommand, state::ShowState}, event::UiEvent, model::ShowModel, start_backend, BackendHandle};
 use tauri::{AppHandle, Emitter, LogicalSize, Manager as _, Size};
 use tokio::sync::{broadcast, watch};
+use uuid::Uuid;
 
 async fn forward_backend_state_and_event(
     app_handle: AppHandle,
@@ -28,6 +31,18 @@ async fn go(handle: tauri::State<'_, BackendHandle>) -> Result<(), String> {
 }
 
 #[tauri::command]
+async fn set_playback_cursor(handle: tauri::State<'_, BackendHandle>, cue_id: String) -> Result<(), String> {
+    match Uuid::from_str(&cue_id) {
+        Ok(cursor) => {
+            handle.controller_tx.send(ControllerCommand::SetPlaybackCursor { cue_id: cursor }).await.map_err(|e| e.to_string())
+        },
+        Err(e) => {
+            Err(e.to_string())
+        }
+    }
+}
+
+#[tauri::command]
 async fn get_show_model(handle: tauri::State<'_, BackendHandle>) -> Result<ShowModel, String> {
     Ok(handle.model_handle.read().await.clone())
 }
@@ -47,7 +62,7 @@ pub fn run() {
             main_window.set_size(Size::Logical(LogicalSize{ width: 1280.0, height: 720.0})).unwrap();
             Ok(())
         })
-        .invoke_handler(tauri::generate_handler![go, get_show_model])
+        .invoke_handler(tauri::generate_handler![go, get_show_model, set_playback_cursor])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
