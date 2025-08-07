22a0c458b2db700f861927db758dbbbdbf66d3ff
Keinsleif <mountaindull@gmail.com>
2025-08-08 02:03:18 +0900

    [update] playback state on button

--- sbsp_tauri/src/components/ToolHeader.vue
+++ sbsp_tauri/src/components/ToolHeader.vue
@@ -3,19 +3,19 @@
     <v-sheet class="d-flex flex-row ma-0 w-100">
       <div class="d-flex flex-column ma-0 flex-grow-1">
         <v-sheet class="pa-2 rounded mb-1 border-md" height="42px">
-          {{playbackCue != null ? playbackCue.number + "・" + playbackCue.name : ""}}
+          {{selectedCue != null ? selectedCue.number + "・" + selectedCue.name : ""}}
         </v-sheet>
         <v-sheet
           class="pa-2 pb-0 rounded border-md text-pre-wrap overflow-auto"
           height="64px"
-        >{{playbackCue != null ? playbackCue.notes : ""}}</v-sheet>
+        >{{selectedCue != null ? selectedCue.notes : ""}}</v-sheet>
       </div>
     </v-sheet>
     <v-sheet class="d-flex flex-columns ga-4">
-      <v-btn-group variant="tonal" divided class="ml-0 mr-auto">
-        <v-btn :icon="mdiStop"></v-btn>
-        <v-btn :icon="mdiPlay"></v-btn>
-        <v-btn :icon="mdiPause"></v-btn>
+      <v-btn-group variant="flat" divided border class="ml-0 mr-auto">
+        <v-btn :icon="mdiStop" active-color="error"></v-btn>
+        <v-btn :icon="mdiPlay" :active="isCueStatus('Playing')" active-color="success" @click="invoke('go')"></v-btn>
+        <v-btn :icon="mdiPause" :active="isCueStatus('Paused')" active-color="warning" :class="[isCueStatus('Paused') ? $style['pause-blink'] : '']"></v-btn>
       </v-btn-group>
       <v-btn-group variant="tonal" divided>
         <v-btn :icon="mdiVolumeHigh"></v-btn>
@@ -50,12 +50,39 @@ import {
 import { useShowModel } from "../stores/showmodel";
 import { computed } from "vue";
 import { useUiState } from "../stores/uistate";
+import { useShowState } from "../stores/showstate";
+import { PlaybackStatus } from "../types/state/PlaybackStatus";
+import { invoke } from "@tauri-apps/api/core";
 
 const showModel = useShowModel();
+const showState = useShowState();
 const uiState = useUiState();
 
-const playbackCue = computed(() => {
+const selectedCue = computed(() => {
   return uiState.selected != null ? showModel.cues[uiState.selected] : null;
+})
+
+const isCueStatus = computed(() => (status: PlaybackStatus) => {
+  if (uiState.selected != null) {
+    const activeCue = showState.activeCues[showModel.cues[uiState.selected].id];
+    if (activeCue != null) {
+      return activeCue.status == status;
+    }
+  }
+  return false;
 })
 </script>
 
+<style lang="css" module>
+.pause-blink {
+  animation: flash 1s ease infinite;
+}
+@keyframes flash {
+  0%,100% {
+    opacity: 1;
+  }
+  50% {
+    opacity: 0;
+  }
+}
+</style>
