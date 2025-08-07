ee2d968db94b68b7485435b5213c422d2308b491
Keinsleif <mountaindull@gmail.com>
2025-08-07 18:34:12 +0900

    [add] model change command

--- sbsp_tauri/src-tauri/src/lib.rs
+++ sbsp_tauri/src-tauri/src/lib.rs
@@ -1,6 +1,6 @@
 use std::str::FromStr;
 
-use sbsp_backend::{controller::{ControllerCommand, state::ShowState}, event::UiEvent, model::ShowModel, start_backend, BackendHandle};
+use sbsp_backend::{controller::{state::ShowState, ControllerCommand}, event::UiEvent, model::{cue::Cue, ShowModel}, start_backend, BackendHandle};
 use tauri::{AppHandle, Emitter, LogicalSize, Manager as _, Size};
 use tokio::sync::{broadcast, watch};
 use uuid::Uuid;
@@ -47,6 +47,40 @@ async fn get_show_model(handle: tauri::State<'_, BackendHandle>) -> Result<ShowM
     Ok(handle.model_handle.read().await.clone())
 }
 
+#[tauri::command]
+async fn update_cue(handle: tauri::State<'_, BackendHandle>, cue: Cue) -> Result<(), String> {
+    handle.model_handle.update_cue(cue).await.map_err(|e| e.to_string())
+}
+
+#[tauri::command]
+async fn add_cue(handle: tauri::State<'_, BackendHandle>, cue: Cue, at_index: usize) -> Result<(), String> {
+    handle.model_handle.add_cue(cue, at_index).await.map_err(|e| e.to_string())
+}
+
+#[tauri::command]
+async fn remove_cue(handle: tauri::State<'_, BackendHandle>, cue_id: &str) -> Result<(), String> {
+    match Uuid::from_str(cue_id) {
+        Ok(cue_uuid) => {
+            handle.model_handle.remove_cue(cue_uuid).await.map_err(|e| e.to_string())
+        },
+        Err(e) => {
+            Err(e.to_string())
+        }
+    }
+}
+
+#[tauri::command]
+async fn move_cue(handle: tauri::State<'_, BackendHandle>, cue_id: &str, to_index: usize) -> Result<(), String> {
+    match Uuid::from_str(cue_id) {
+        Ok(cue_uuid) => {
+            handle.model_handle.move_cue(cue_uuid, to_index).await.map_err(|e| e.to_string())
+        },
+        Err(e) => {
+            Err(e.to_string())
+        }
+    }
+}
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
@@ -62,7 +96,15 @@ pub fn run() {
             main_window.set_size(Size::Logical(LogicalSize{ width: 1280.0, height: 720.0})).unwrap();
             Ok(())
         })
-        .invoke_handler(tauri::generate_handler![go, get_show_model, set_playback_cursor])
+        .invoke_handler(tauri::generate_handler![
+            go,
+            get_show_model,
+            set_playback_cursor,
+            update_cue,
+            add_cue,
+            remove_cue,
+            move_cue,
+        ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
