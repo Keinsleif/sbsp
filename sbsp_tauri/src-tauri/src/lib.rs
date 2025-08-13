18e85eda1dc69bbc8e3c58a40faa7d5939157e9e
Keinsleif <mountaindull@gmail.com>
2025-08-13 17:13:35 +0900

    [update] dependencies

--- sbsp_tauri/src-tauri/src/lib.rs
+++ sbsp_tauri/src-tauri/src/lib.rs
@@ -147,7 +147,6 @@ async fn move_cue(
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
-    env_logger::init();
     tauri::Builder::default()
         .plugin(tauri_plugin_log::Builder::new().build())
         .plugin(tauri_plugin_dialog::init())
