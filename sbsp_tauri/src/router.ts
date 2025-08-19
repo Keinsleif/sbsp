86d4ed33d5022d978eb8c49a9ec9167498afd4c6
Keinsleif <mountaindull@gmail.com>
2025-08-19 14:30:14 +0900

    [fix] history mode for url to work

--- sbsp_tauri/src/router.ts
+++ sbsp_tauri/src/router.ts
@@ -1,10 +1,14 @@
-import { createMemoryHistory, createRouter } from 'vue-router';
+import { createWebHistory, createRouter } from 'vue-router';
 import MainView from './MainView.vue';
+import SettingsView from './SettingsView.vue';
 
-const routes = [{ path: '/', component: MainView }];
+const routes = [
+  { path: '/', component: MainView },
+  { path: '/settings', component: SettingsView },
+];
 
 const router = createRouter({
-  history: createMemoryHistory(),
+  history: createWebHistory(),
   routes,
 });
 
