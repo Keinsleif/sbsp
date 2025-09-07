dae4daf31db7c71b41a0fc56431e53a9fd3ece2a
Keinsleif <mountaindull@gmail.com>
2025-09-07 19:14:22 +0900

    [update] some active cue appearance

--- sbsp_tauri/src/components/SideBar.vue
+++ sbsp_tauri/src/components/SideBar.vue
@@ -6,19 +6,14 @@
   <v-tabs-window v-model="uiState.sideBarTab">
     <v-tabs-window-item value="activeCues" class="overflow-y-auto" transition="false" reverse-transition="false">
       <template v-for="(activeCue, cue_id) in showState.activeCues" :key="cue_id">
-        <v-card v-if="activeCue != null" class="border">
-          <v-card-title class="text-subtitle-1 pb-0">
-            {{
-              showModel.cues.find((cue) => cue.id == cue_id) != null
-                ? showModel.cues.find((cue) => cue.id == cue_id)?.number +
-                  '・' +
-                  (showModel.cues.find((cue) => cue.id == cue_id)?.name != null
-                    ? showModel.cues.find((cue) => cue.id == cue_id)?.name
-                    : buildCueName(showModel.cues.find((cue) => cue.id == cue_id) || null))
-                : ''
-            }}
-          </v-card-title>
-          <v-card-subtitle class="pa-0 d-flex justify-space-between">
+        <v-sheet v-if="activeCue != null" class="d-flex flex-column border">
+          <v-sheet class="d-flex flex-row align-center justify-space-between pl-3 pr-3 pt-2">
+            <span>{{ buildTitle(cue_id) }}</span>
+            <v-icon
+              :icon="activeCue.params.type == 'audio' && activeCue.params.repeating ? mdiRepeat : undefined"
+            ></v-icon>
+          </v-sheet>
+          <v-sheet class="pa-0 d-flex flex-row justify-space-between">
             <v-list-item density="compact">
               <v-list-item-subtitle>
                 {{
@@ -37,19 +32,20 @@
                 }}
               </v-list-item-subtitle>
             </v-list-item>
-          </v-card-subtitle>
+          </v-sheet>
           <v-progress-linear
             :color="activeCue.status == 'Paused' ? 'warning' : 'primary'"
             :model-value="activeCue != null ? (activeCue.position * 100) / activeCue.duration : 0"
             height="16"
           ></v-progress-linear>
-        </v-card>
+        </v-sheet>
       </template>
     </v-tabs-window-item>
   </v-tabs-window>
 </template>
 
 <script setup lang="ts">
+import { mdiRepeat } from '@mdi/js';
 import { useShowModel } from '../stores/showmodel';
 import { useShowState } from '../stores/showstate';
 import { useUiState } from '../stores/uistate';
@@ -59,4 +55,19 @@ import { buildCueName, secondsToFormat } from '../utils';
 const showModel = useShowModel();
 const showState = useShowState();
 const uiState = useUiState();
+
+const buildTitle = (cue_id: string) => {
+  const activeCue = showModel.cues.find((cue) => cue.id == cue_id);
+  if (activeCue == null) return;
+  let result = '';
+  if (activeCue.number.trim() != '') {
+    result = activeCue.number + '・';
+  }
+  if (activeCue.name != null) {
+    result = result + activeCue.name;
+  } else {
+    result = result + buildCueName(activeCue);
+  }
+  return result;
+};
 </script>
