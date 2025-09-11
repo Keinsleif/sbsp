c0786321393625093a40548fb8b148fd599c7309
Keinsleif <mountaindull@gmail.com>
2025-09-11 22:13:23 +0900

    [fix] waveform rendering

--- sbsp_tauri/src/components/editor/WaveformViewer.vue
+++ sbsp_tauri/src/components/editor/WaveformViewer.vue
@@ -10,16 +10,18 @@
       height="116px"
       @mousedown="seek($event)"
     >
+      <rect :class="$style.waveform" x="0" y="58" height="1" :width="compressedWaveform.length"></rect>
       <g :transform="`scale(1, ${Math.pow(10, props.volume / 20)}) translate(0, 12)`" transform-origin="center">
-        <rect
-          :class="$style.waveform"
-          v-for="(sample, index) in compressedWaveform"
-          :key="index"
-          :x="index"
-          :y="(1 - sample) * 46"
-          width="1"
-          :height="sample * 92"
-        ></rect>
+        <template v-for="(sample, index) in compressedWaveform" :key="index">
+          <rect
+            v-if="sample != 0"
+            :class="$style.waveform"
+            :x="index"
+            :y="(1 - sample) * 46"
+            width="1"
+            :height="sample * 92"
+          ></rect>
+        </template>
       </g>
       <rect :x="nonNullStartTime * (compressedWaveform.length - 1)" y="0" width="2" height="116" fill="blue"></rect>
       <rect :x="nonNullEndTime * (compressedWaveform.length - 1) - 1" y="0" width="2" height="116" fill="blue"></rect>
@@ -91,37 +93,39 @@ const position = computed(() => {
     return 0;
   }
 });
-const compressedWaveform = computed<number[]>((oldValue) => {
-  if (svgRef.value != null && props.targetId != null && props.targetId in assetResult.results) {
-    let result = [] as number[];
-    let source = assetResult.results[props.targetId].waveform;
-    if (source == null || svgRef.value.clientWidth < 1) {
-      return oldValue != null ? oldValue : [0];
-    }
-    const window = source.length / (svgRef.value.clientWidth - 1);
-    let loop_count = 0;
-    for (let i = 0; i < source.length || loop_count > 10000; i += window) {
-      loop_count++;
-      let start = Math.floor(i);
-      let end = Math.floor(i + window);
-      let slice;
-      if (start == end) {
-        result.push(source[start]);
-        continue;
-      }
-      if (source.length < end) {
-        slice = source.slice(start);
-      } else {
-        slice = source.slice(start, end);
-      }
-      result.push(slice.reduce((a, b) => Math.max(a, b), -Infinity));
-      // result.push(slice.reduce((a, b) => Math.min(a, b), Infinity));
-      // result.push(slice.reduce((acc, cur) => acc + cur, 0) / slice.length);
-    }
-    return result;
-  } else {
-    return oldValue != null ? oldValue : [0];
+const compressedWaveform = computed<number[]>(() => {
+  if (svgRef.value == null || svgRef.value.clientWidth < 1) {
+    return [0];
   }
+  if (props.targetId == null || !(props.targetId in assetResult.results)) {
+    return Array(svgRef.value.clientWidth).fill(0);
+  }
+  let result = [] as number[];
+  let source = assetResult.results[props.targetId].waveform;
+  if (source == null) {
+    return Array(svgRef.value.clientWidth).fill(0);
+  }
+  const window = source.length / (svgRef.value.clientWidth - 1);
+  let loop_count = 0;
+  for (let i = 0; i < source.length || loop_count > 10000; i += window) {
+    loop_count++;
+    let start = Math.floor(i);
+    let end = Math.floor(i + window);
+    if (start == end) {
+      result.push(source[start]);
+      continue;
+    }
+    let slice;
+    if (source.length < end) {
+      slice = source.slice(start);
+    } else {
+      slice = source.slice(start, end);
+    }
+    result.push(slice.reduce((a, b) => Math.max(a, b), -Infinity));
+    // result.push(slice.reduce((a, b) => Math.min(a, b), Infinity));
+    // result.push(slice.reduce((acc, cur) => acc + cur, 0) / slice.length);
+  }
+  return result;
 });
 
 const seek = (event: MouseEvent) => {
