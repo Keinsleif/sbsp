1d765121b3b621bd89cf68d40cc94000137328db
Keinsleif <mountaindull@gmail.com>
2025-09-22 22:20:59 +0900

    [fix] i18n

--- sbsp_tauri/src/i18n.ts
+++ sbsp_tauri/src/i18n.ts
@@ -1,4 +1,5 @@
 import { createI18n } from 'vue-i18n';
+import { en, ja } from 'vuetify/locale';
 
 export const i18n = createI18n({
   legacy: false,
@@ -6,9 +7,21 @@ export const i18n = createI18n({
   fallbackLocale: 'en',
   messages: {
     en: {
+      $vuetify: {
+        ...en,
+      },
       notification: {
         modelLoaded: 'Show model loaded.',
         modelSaved: 'Show model saved.',
+      },
+    },
+    ja: {
+      $vuetify: {
+        ...ja,
+      },
+      notification: {
+        modelLoaded: '正常にロードされました',
+        modelSaved: '正常に保存されました',
       },
     },
   },
