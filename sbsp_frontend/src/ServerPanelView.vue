55d454d5ab07f75fa22558a4c5207d9f86c5da50
Keinsleif <mountaindull@gmail.com>
2025-11-16 21:24:50 +0900

    [add] server panel i18n

--- sbsp_frontend/src/ServerPanelView.vue
+++ sbsp_frontend/src/ServerPanelView.vue
@@ -3,9 +3,9 @@
     <div class="d-flex flex-column pa-4 ga-4">
       <div>
         <span>
-          Status :
+          {{ t('view.server.status') }} :
           <span :class="isRunning == null ? '' : isRunning ? 'text-green' : 'text-red'">
-            {{ isRunning == null ? '' : isRunning ? 'Running' : 'Stopped' }}
+            {{ isRunning == null ? '' : isRunning ? t('view.server.running') : t('view.server.stopped') }}
           </span>
         </span>
       </div>
@@ -13,14 +13,14 @@
         v-model="server_port"
         :disabled="isRunning"
         class="flex-grow-0"
-        label="Port"
+        :label="t('view.server.port')"
         width="100px"
         @update="invoke('set_server_port', { port: parseInt(server_port) }).catch((e) => console.error(e))"
       ></text-input>
       <v-checkbox
         v-model="is_discoverable"
         :disabled="isRunning"
-        label="Discoverable"
+        :label="t('view.server.discoverable')"
         density="compact"
         hide-details
         @update="invoke('set_discovery_option', { discoveryOption: server_name }).catch((e) => console.error(e))"
@@ -30,7 +30,7 @@
         :disabled="!is_discoverable || isRunning"
         align-input="left"
         class="flex-grow-0"
-        label="Server Name"
+        :label="t('view.server.serverName')"
         width="480px"
         @update="invoke('set_discovery_option', { discoveryOption: server_name }).catch((e) => console.error(e))"
       ></text-input>
@@ -39,11 +39,11 @@
     <v-footer class="flex-grow-0 d-flex align-center ml-0 mr-0 w-100 mt-auto ga-3">
       <v-btn
         class="ml-auto"
-        :text="isRunning ? 'Stop' : 'Start'"
+        :text="isRunning ? t('view.server.stop') : t('view.server.start')"
         :color="isRunning ? 'red' : 'green'"
         @click="toggleServer"
       ></v-btn>
-      <v-btn text="Close" variant="outlined" @click="close"></v-btn>
+      <v-btn :text="t('general.close')" variant="outlined" @click="close"></v-btn>
     </v-footer>
   </div>
 </template>
@@ -54,6 +54,9 @@ import { listen, type UnlistenFn } from '@tauri-apps/api/event';
 import { onMounted, onUnmounted, ref, watch } from 'vue';
 import TextInput from './components/input/TextInput.vue';
 import { getCurrentWebviewWindow } from '@tauri-apps/api/webviewWindow';
+import { useI18n } from 'vue-i18n';
+
+const { t } = useI18n();
 
 const isRunning = ref<boolean | null>(null);
 const server_port = ref<string>('');
