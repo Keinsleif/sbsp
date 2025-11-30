d11a03138a6682671fbd5c9128396e619b7566ab
Keinsleif <mountaindull@gmail.com>
2025-11-30 15:51:19 +0900

    [update] remove unused code

--- sbsp_remote/src/command.rs
+++ sbsp_remote/src/command.rs
@@ -1,8 +1,8 @@
-use std::{path::PathBuf, time::Duration};
+use std::path::PathBuf;
 
 use super::AppState;
 use tauri::{Listener, ipc::Channel};
-use tokio::{sync::oneshot, time::interval};
+use tokio::sync::oneshot;
 
 pub mod client;
 pub mod controller;
@@ -50,6 +50,6 @@ pub async fn pick_audio_assets(app_handle: tauri::AppHandle) -> Result<Vec<PathB
 }
 
 #[tauri::command]
-pub async fn listen_level_meter(state: tauri::State<'_, AppState>, level_listener: Channel<(f32, f32)>) -> Result<(), String> {
+pub async fn listen_level_meter(_state: tauri::State<'_, AppState>, _level_listener: Channel<(f32, f32)>) -> Result<(), String> {
     Err("Level Meter is not implemented on remote.".into())
 }
