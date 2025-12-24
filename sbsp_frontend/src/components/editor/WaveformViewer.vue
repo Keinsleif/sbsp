ebf54ce1b6e4b284761497887402c15304eb841f
Keinsleif <mountaindull@gmail.com>
2025-12-24 14:20:58 +0900

    [update] waveform optimization

--- sbsp_frontend/src/components/editor/WaveformViewer.vue
+++ sbsp_frontend/src/components/editor/WaveformViewer.vue
@@ -1,55 +1,42 @@
 <template>
-  <div style="height: 120px" class="w-100 border-md">
+  <div :style="{ height: `${props.heightPx}px` }" class="w-100 border-md">
     <v-sheet
       v-show="!isOutside"
       v-if="props.targetId != null && svgRef != null && parent != null"
-      :style="{
-        position: 'absolute',
-        top: `${mouseY - parent.getBoundingClientRect().top - 10}px`,
-        left: `${parent.getBoundingClientRect().right - mouseX > 150 ? mouseX - parent.getBoundingClientRect().left + 15 : mouseX - parent.getBoundingClientRect().left - 120}px`,
-      }"
+      :style="tooltipStyle"
       class="pl-1 pr-1 rounded text-caption"
     >
-      {{ buildTooltipText() }}
+      {{ tooltipText }}
     </v-sheet>
     <svg
       ref="svg"
       preserveAspectRatio="none"
       v-show="props.targetId != null"
       xmlns="http://www.w3.org/2000/svg"
-      :viewBox="`0 0 ${compressedWaveform.length} 116`"
+      :viewBox="`0 0 ${svgWidth} ${contentHeight}`"
       width="100%"
-      height="116px"
+      :height="`${contentHeight}px`"
       @mousedown="seek($event)"
     >
-      <rect :class="$style.waveform" x="0" y="58" height="1" :width="compressedWaveform.length"></rect>
-      <g :transform="`scale(1, ${Math.pow(10, props.volume / 20)}) translate(0, 12)`" transform-origin="center">
-        <template v-for="(sample, index) in compressedWaveform" :key="index">
-          <rect
-            v-if="sample != 0"
-            :class="$style.waveform"
-            :x="index"
-            :y="(1 - sample) * 46"
-            width="1"
-            :height="sample * 92"
-          ></rect>
-        </template>
-      </g>
-      <rect :x="nonNullStartTime * (compressedWaveform.length - 1)" y="0" width="2" height="116" fill="blue"></rect>
-      <rect :x="nonNullEndTime * (compressedWaveform.length - 1) - 1" y="0" width="2" height="116" fill="blue"></rect>
+      <rect :class="$style.waveform" x="0" :y="contentHeight / 2" height="1" :width="svgWidth"></rect>
+      <path
+        :d="waveformPath"
+        :transform="`scale(1, ${Math.pow(10, props.volume / 20)}) translate(0, ${contentHeight * 0.125})`"
+        :class="$style.waveform"
+        transform-origin="center"
+      />
+      <rect :x="startPos" y="0" width="2" :height="contentHeight" fill="blue"></rect>
+      <rect :x="endPos" y="0" width="2" :height="contentHeight" fill="blue"></rect>
       <rect
         v-show="position != 0"
         :style="{
-          transform: `translateX(${
-            position * ((nonNullEndTime - nonNullStartTime) * (compressedWaveform.length - 1)) +
-            nonNullStartTime * compressedWaveform.length
-          }px)`,
-          transition: 'transform 120ms linear',
+          transform: `translateX(${currentPlayPos}px)`,
+          transition: 'transform 150ms linear',
         }"
         x="0"
         y="0"
         width="2"
-        height="116"
+        :height="contentHeight"
         fill="yellow"
       ></rect>
     </svg>
@@ -57,10 +44,9 @@
 </template>
 
 <script setup lang="ts">
-import { computed, useTemplateRef } from 'vue';
+import { computed, StyleValue, useTemplateRef } from 'vue';
 import { useAssetResult } from '../../stores/assetResult';
 import { useShowState } from '../../stores/showstate';
-import type { PlaybackStatus } from '../../types/PlaybackStatus';
 import { invoke } from '@tauri-apps/api/core';
 import { useElementSize, useMouseInElement, useParentElement } from '@vueuse/core';
 import { secondsToFormat } from '../../utils';
@@ -74,124 +60,122 @@ const props = withDefaults(
     volume?: number;
     startTime?: number | null;
     endTime?: number | null;
+    heightPx?: number;
   }>(),
   {
     targetId: null,
     volume: 0,
     startTime: 0,
     endTime: 1,
+    heightPx: 75,
   },
 );
 
+const contentHeight = computed(() => props.heightPx - 4);
+
 const nonNullStartTime = computed<number>(() => {
-  if (props.targetId == null) {
-    return 0;
-  }
   const duration = assetResult.get(props.targetId)?.duration;
   return props.startTime != null && duration != null ? props.startTime / duration : 0;
 });
 
 const nonNullEndTime = computed<number>(() => {
-  if (props.targetId == null) {
-    return 1;
-  }
   const duration = assetResult.get(props.targetId)?.duration;
   return props.endTime != null && duration != null ? props.endTime / duration : 1;
 });
+
+const startPos = computed<number>(() => nonNullStartTime.value * (svgWidth.value - 1));
+const endPos = computed<number>(() => nonNullEndTime.value * (svgWidth.value - 1) - 1);
 
 const svgRef = useTemplateRef('svg');
 const { width: svgWidth } = useElementSize(svgRef);
 const parent = useParentElement();
 const position = computed<number>(() => {
-  if (props.targetId != null && props.targetId in showState.activeCues) {
-    const activeCue = showState.activeCues[props.targetId];
-    if (
-      activeCue != null &&
-      (['Playing', 'Paused', 'Loaded', 'Stopping'] as PlaybackStatus[]).includes(activeCue.status) &&
-      activeCue.duration !== 0
-    ) {
-      return activeCue.position / activeCue.duration;
-    } else {
-      return 0;
-    }
+  if (props.targetId == null) return 0;
+  const activeCue = showState.activeCues[props.targetId];
+  if (activeCue != null && activeCue.duration !== 0) {
+    return activeCue.position / activeCue.duration;
   } else {
     return 0;
   }
 });
-const compressedWaveform = computed<number[]>(() => {
+
+const currentPlayPos = computed(() => {
+  const range = nonNullEndTime.value - nonNullStartTime.value;
+  return (nonNullStartTime.value + position.value * range) * (svgWidth.value - 1);
+});
+
+const waveformPath = computed<string>(() => {
   if (svgWidth.value < 1) {
-    return [0];
+    return '';
   }
   if (props.targetId == null) {
-    return Array(svgWidth.value).fill(0);
+    return '';
   }
-  let result = [] as number[];
-  let source = assetResult.get(props.targetId)?.waveform;
+  const source = assetResult.get(props.targetId)?.waveform;
   if (source == null) {
-    return Array(svgWidth.value).fill(0);
+    return '';
   }
-  const window = source.length / (svgWidth.value - 1);
-  let loop_count = 0;
-  for (let i = 0; i < source.length || loop_count > 10000; i += window) {
-    loop_count++;
-    let start = Math.floor(i);
-    let end = Math.floor(i + window);
-    if (start == end) {
-      result.push(source[start]);
-      continue;
+
+  let result = '';
+  const amp = contentHeight.value * 0.375;
+
+  const samplePerPixel = source.length / svgWidth.value;
+  for (let i = 0; i < svgWidth.value; i++) {
+    let start = Math.floor(i * samplePerPixel);
+    let end = Math.floor((i + 1) * samplePerPixel);
+
+    let max = source[start];
+    for (let j = start; j < end; j++) {
+      if (source[j] > max) max = source[j];
     }
-    let slice;
-    if (source.length < end) {
-      slice = source.slice(start);
-    } else {
-      slice = source.slice(start, end);
+    if (max > 0) {
+      result += `M${i},${(1 - max) * amp}v${2 * amp * max}`;
     }
-    result.push(slice.reduce((a, b) => Math.max(a, b), -Infinity));
-    // result.push(slice.reduce((a, b) => Math.min(a, b), Infinity));
-    // result.push(slice.reduce((acc, cur) => acc + cur, 0) / slice.length);
   }
   return result;
 });
 const { x: mouseX, y: mouseY, elementX, isOutside } = useMouseInElement(svgRef);
 
 const seek = (event: MouseEvent) => {
-  if (
-    svgRef.value == null ||
-    svgRef.value.clientWidth < 1 ||
-    props.targetId == null ||
-    !(props.targetId in showState.activeCues)
-  ) {
+  if (props.targetId == null) {
     return;
   }
   const activeCue = showState.activeCues[props.targetId];
-  if (
-    activeCue == null ||
-    !(['Playing', 'Paused', 'Loaded', 'Stopping'] as PlaybackStatus[]).includes(activeCue.status)
-  ) {
+  if (activeCue == null) {
     return;
   }
   const position =
-    (event.offsetX - nonNullStartTime.value * svgRef.value.clientWidth) /
-    (svgRef.value.clientWidth * (nonNullEndTime.value - nonNullStartTime.value));
+    (event.offsetX - nonNullStartTime.value * svgWidth.value) /
+    (svgWidth.value * (nonNullEndTime.value - nonNullStartTime.value));
   if (position > 0 && position < 1) {
     invoke('seek_to', { cueId: props.targetId, position: position * activeCue.duration });
   }
 };
 
-const buildTooltipText = () => {
-  if (props.targetId == null || svgRef.value == null) {
+const tooltipText = computed(() => {
+  if (props.targetId == null) {
     return '--:--.-- / --:--.--';
   }
   const duration = assetResult.get(props.targetId)?.duration;
   if (duration == null) {
     return '--:--.-- / --:--.--';
   }
-  return `${secondsToFormat((elementX.value / svgRef.value.clientWidth) * duration)} / ${secondsToFormat(duration)}`;
-};
+  return `${secondsToFormat((elementX.value / svgWidth.value) * duration)} / ${secondsToFormat(duration)}`;
+});
+
+const tooltipStyle = computed<StyleValue>(() => {
+  if (parent.value == null) return {};
+  const parentRect = parent.value.getBoundingClientRect();
+  return {
+    position: 'absolute',
+    top: `${mouseY.value - parentRect.top - 10}px`,
+    left: `${parentRect.right - mouseX.value > 150 ? mouseX.value - parentRect.left + 15 : mouseX.value - parentRect.left - 120}px`,
+  };
+});
 </script>
 
 <style lang="css" module>
 .waveform {
-  fill: rgb(var(--v-theme-surface-variant), 0.8);
+  stroke: rgb(var(--v-theme-surface-variant), 0.8);
 }
 </style>
