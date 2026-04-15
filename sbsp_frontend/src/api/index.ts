99c39282094edcfddecbc26ded6635b0c9b77e83
Keinsleif <mountaindull@gmail.com>
2026-04-15 21:28:08 +0900

    update: add type on  api.target

--- sbsp_frontend/src/api/index.ts
+++ sbsp_frontend/src/api/index.ts
@@ -2,7 +2,7 @@ import { IBackendAdapter } from './interface';
 import { useTauriApi } from './tauri';
 import { useWebsocketApi } from './websocket';
 
-export const target = import.meta.env.VITE_APP_TARGET;
+export const target: 'websocket' | 'tauri' = import.meta.env.VITE_APP_TARGET;
 
 export const side: 'host' | 'remote'
   = target == 'websocket' ? 'remote' : import.meta.env.VITE_APP_SIDE == 'host' ? 'host' : 'remote';
