e7119d8c64bb5199ea40a5de9e03d75e16f2eb70
Keinsleif <mountaindull@gmail.com>
2026-04-20 19:53:03 +0900

    add(backend): envelope tests

--- sbsp_backend/src/engine/audio_engine/audio_source/envelope.rs
+++ sbsp_backend/src/engine/audio_engine/audio_source/envelope.rs
@@ -70,3 +70,46 @@ impl Envelope {
         };
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::{engine::audio_engine::audio_source::envelope::Envelope, model::cue::audio::{Decibels, EnvelopeParam}};
+
+    #[test]
+    fn update_normal() {
+        let mut envelope = Envelope::new(vec![
+            EnvelopeParam { start: 0.0, end: 0.1, volume: Decibels::IDENTITY },
+            EnvelopeParam { start: 0.3, end: 0.5, volume: Decibels::from(-10.0) },
+            EnvelopeParam { start: 0.8, end: 1.0, volume: Decibels::IDENTITY },
+        ], 30.0);
+
+        // start point
+        assert_eq!(envelope.update(0.0), Decibels::IDENTITY);
+        // on first segment
+        assert_eq!(envelope.update(30.0 * 0.05), Decibels::IDENTITY);
+        // between first and second segment
+        assert_eq!(envelope.update(30.0 * 0.2), Decibels::from(-5.0));
+        // end of second segment
+        assert_eq!(envelope.update(30.0 * 0.5), Decibels::from(-10.0));
+        // end point
+        assert_eq!(envelope.update(30.0), Decibels::IDENTITY); 
+    }
+
+    #[test]
+    fn seek() {
+        let mut envelope = Envelope::new(vec![
+            EnvelopeParam { start: 0.0, end: 0.1, volume: Decibels::IDENTITY },
+            EnvelopeParam { start: 0.3, end: 0.5, volume: Decibels::from(-10.0) },
+            EnvelopeParam { start: 0.8, end: 1.0, volume: Decibels::IDENTITY },
+        ], 30.0);
+
+        // end of second segment
+        assert_eq!(envelope.update(30.0 * 0.5), Decibels::from(-10.0));
+
+        // seek
+        envelope.seek(30.0 * 0.05);
+
+        // between first and second segment
+        assert_eq!(envelope.update(30.0 * 0.2), Decibels::from(-5.0));
+    }
+}
