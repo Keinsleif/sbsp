8e681e1558bc6b76a010eb52318d4ebde8224cee
Keinsleif <mountaindull@gmail.com>
2026-04-30 20:34:03 +0900

    fix(frontend): eslint configuration

--- sbsp_frontend/eslint.config.ts
+++ sbsp_frontend/eslint.config.ts
@@ -1,27 +1,26 @@
-import js from '@eslint/js';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
 import pluginVue from 'eslint-plugin-vue';
-import { defineConfig, globalIgnores } from 'eslint/config';
 import stylistic from '@stylistic/eslint-plugin';
+import { defineConfig, globalIgnores } from 'eslint/config';
 
 export default defineConfig([
   globalIgnores(['node_modules/', 'dist/', 'src/types/']),
   {
-    files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
-    plugins: { js },
-    extends: ['js/recommended'],
-    languageOptions: { globals: globals.browser },
+    files: ['**/*.{js,mjs,cjs,ts,mts,cts,vue}'],
+    languageOptions: {
+      globals: globals.browser
+    }
   },
   tseslint.configs.recommended,
-  stylistic.configs.customize({
-    semi: true,
-    braceStyle: '1tbs',
-  }),
   pluginVue.configs['flat/strongly-recommended'],
   {
     files: ['**/*.vue'],
-    languageOptions: { parserOptions: { parser: tseslint.parser } },
+    languageOptions: {
+      parserOptions: {
+        parser: tseslint.parser
+      },
+    },
     rules: {
       'vue/max-attributes-per-line': ['error', {
         singleline: {
@@ -33,4 +32,8 @@ export default defineConfig([
       }],
     },
   },
+  stylistic.configs.customize({
+    semi: true,
+    braceStyle: '1tbs',
+  }),
 ]);
