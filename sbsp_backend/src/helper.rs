fc3f9f533154da4db7f58cd717d86de4e7491964
Keinsleif <mountaindull@gmail.com>
2026-05-02 13:04:47 +0900

    fix(backend): helper info struct

--- sbsp_backend/src/helper.rs
+++ sbsp_backend/src/helper.rs
@@ -5,26 +5,27 @@ use rodio::{DeviceTrait, cpal::{SampleFormat, SupportedBufferSize, traits::HostT
 use serde::{Deserialize, Serialize};
 
 const FRAME_SIZES: &[u32] = &[32, 64, 128, 256, 512, 1024, 2048, 4096];
+const SUPPORTED_SAMPLE_RATE: u32 = 192000;
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Debug)]
 pub struct SupportedHardware {
-    default: String,
-    devices: HashMap<String, DeviceInformation>
+    pub default: String,
+    pub devices: HashMap<String, DeviceInformation>
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Debug)]
 pub struct DeviceInformation {
-    name: String,
-    supported_configs: Vec<FrameConfig>,
-    default_channel_count: u16,
-    default_sample_rate: u32,
+    pub name: String,
+    pub supported_configs: Vec<FrameConfig>,
+    pub default_channel_count: u16,
+    pub default_sample_rate: u32,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Debug)]
 pub struct FrameConfig {
-    channel_count: u16,
-    sample_rates: BTreeSet<u32>,
-    buffer_sizes: BTreeSet<u32>,
+    pub channel_count: u16,
+    pub sample_rates: BTreeSet<u32>,
+    pub buffer_sizes: BTreeSet<u32>,
 }
 
 fn get_buffer_sizes(buf_conf: SupportedBufferSize) -> BTreeSet<u32> {
@@ -48,12 +49,13 @@ pub fn get_supported_hardware() -> Result<SupportedHardware> {
     let mut hardwares = HashMap::new();
     for device in devices {
         if let Ok(id) = device.id()
-        && let Ok(name) = device.description()
+        && let Ok(description) = device.description()
         && let Ok(supported_confs) = device.supported_output_configs()
         && let Ok(default_config) = device.default_output_config() {
             let mut configs = HashMap::new();
             for config in supported_confs {
-                if config.sample_format() != SampleFormat::F32 {
+                if config.sample_format() != SampleFormat::F32
+                || config.max_sample_rate() > SUPPORTED_SAMPLE_RATE {
                     continue;
                 }
                 let entry = configs.entry(config.channels()).or_insert((BTreeSet::new(), *(config.buffer_size())));
@@ -61,7 +63,7 @@ pub fn get_supported_hardware() -> Result<SupportedHardware> {
             }
             if !configs.is_empty() {
                 hardwares.insert(id.to_string(), DeviceInformation {
-                    name: name.to_string(),
+                    name: description.name().to_string(),
                     supported_configs: configs.into_iter().map(|(channel_count, (sample_rates, buffer_sizes))| {
                         FrameConfig {
                             channel_count,
