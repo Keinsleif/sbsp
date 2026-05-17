eaf54baea51476da70db955adcc3dd270d68575a
Keinsleif <mountaindull@gmail.com>
2026-05-17 20:35:26 +0900

    update(frontend): disable hotkey with view mode

--- sbsp_frontend/src/MainView.vue
+++ sbsp_frontend/src/MainView.vue
@@ -265,44 +265,36 @@ if (api.host) {
   );
 }
 
-const goHotkey = computed(() =>
-  uiSettings.settings.hotkey.playback.go != null ? uiSettings.settings.hotkey.playback.go : undefined,
-);
-const loadHotkey = computed(() =>
-  uiSettings.settings.hotkey.playback.load != null ? uiSettings.settings.hotkey.playback.load : undefined,
-);
-const pauseAndResumeHotkey = computed(() =>
-  uiSettings.settings.hotkey.playback.pauseAndResume != null
-    ? uiSettings.settings.hotkey.playback.pauseAndResume
-    : undefined,
-);
-const pauseAllHotkey = computed(() =>
-  uiSettings.settings.hotkey.playback.pauseAll != null ? uiSettings.settings.hotkey.playback.pauseAll : undefined,
-);
-const resumeAllHotkey = computed(() =>
-  uiSettings.settings.hotkey.playback.resumeAll != null ? uiSettings.settings.hotkey.playback.resumeAll : undefined,
-);
-const stopHotkey = computed(() =>
-  uiSettings.settings.hotkey.playback.stop != null ? uiSettings.settings.hotkey.playback.stop : undefined,
-);
-const stopAllHotkey = computed(() =>
-  uiSettings.settings.hotkey.playback.stopAll != null ? uiSettings.settings.hotkey.playback.stopAll : undefined,
-);
-const seekForwardHotkey = computed(() =>
-  uiSettings.settings.hotkey.playback.seekForward != null
-    ? uiSettings.settings.hotkey.playback.seekForward
-    : undefined,
-);
-const seekBackwardHotkey = computed(() =>
-  uiSettings.settings.hotkey.playback.seekBackward != null
-    ? uiSettings.settings.hotkey.playback.seekBackward
-    : undefined,
-);
-const audioToggleRepeatHotkey = computed(() =>
-  uiSettings.settings.hotkey.audioAction.toggleRepeat != null
-    ? uiSettings.settings.hotkey.audioAction.toggleRepeat
-    : undefined,
-);
+const goHotkey = computed(() => {
+  return (uiState.mode != 'view' && uiSettings.settings.hotkey.playback.go) || undefined;
+});
+const loadHotkey = computed(() => {
+  return (uiState.mode != 'view' && uiSettings.settings.hotkey.playback.load) || undefined;
+});
+const pauseAndResumeHotkey = computed(() => {
+  return (uiState.mode != 'view' && uiSettings.settings.hotkey.playback.pauseAndResume) || undefined;
+});
+const pauseAllHotkey = computed(() => {
+  return (uiState.mode != 'view' && uiSettings.settings.hotkey.playback.pauseAll) || undefined;
+});
+const resumeAllHotkey = computed(() => {
+  return (uiState.mode != 'view' && uiSettings.settings.hotkey.playback.resumeAll) || undefined;
+});
+const stopHotkey = computed(() => {
+  return (uiState.mode != 'view' && uiSettings.settings.hotkey.playback.stop) || undefined;
+});
+const stopAllHotkey = computed(() => {
+  return (uiState.mode != 'view' && uiSettings.settings.hotkey.playback.stopAll) || undefined;
+});
+const seekForwardHotkey = computed(() => {
+  return (uiState.mode != 'view' && uiSettings.settings.hotkey.playback.seekForward) || undefined;
+});
+const seekBackwardHotkey = computed(() => {
+  return (uiState.mode != 'view' && uiSettings.settings.hotkey.playback.seekBackward) || undefined;
+});
+const audioToggleRepeatHotkey = computed(() => {
+  return (uiState.mode != 'view' && uiSettings.settings.hotkey.audioAction.toggleRepeat) || undefined;
+});
 
 useHotkey(
   goHotkey,
