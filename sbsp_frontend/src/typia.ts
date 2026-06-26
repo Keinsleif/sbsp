47e681b94d6a579a8f05b8db08cf94d62b44e67e
Keinsleif <mountaindull@gmail.com>
2026-06-26 18:53:16 +0900

    fix: ts & oxc configs

--- sbsp_frontend/src/typia.ts
+++ sbsp_frontend/src/typia.ts
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: Elastic-2.0
 // Copyright (c) 2025 Keinsleif (https://github.com/Keinsleif)
 
-import typia from 'typia';
+import typia, { type IValidation } from 'typia';
 import type { GlobalHostSettings } from './types/GlobalHostSettings';
 import type { GlobalRemoteSettings } from './types/GlobalRemoteSettings';
 
-export const settingsValidator = typia.createValidate<GlobalHostSettings | GlobalRemoteSettings>();
+export const settingsValidator: ((input: unknown) => IValidation<GlobalHostSettings | GlobalRemoteSettings>) = typia.createValidate<GlobalHostSettings | GlobalRemoteSettings>();
