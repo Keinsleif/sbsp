e2517653f64371cebd69ca40838ca5d8a41737fc
Keinsleif <mountaindull@gmail.com>
2026-06-28 17:49:29 +0900

    add: help text for TextInput

--- sbsp_frontend/src/components/input/TextInput.vue
+++ sbsp_frontend/src/components/input/TextInput.vue
@@ -1,12 +1,14 @@
 <script setup lang="ts">
 import FloatLabel from 'primevue/floatlabel';
 import InputText from 'primevue/inputtext';
+import Message from 'primevue/message';
 import { ref, useId, watch } from 'vue';
 
 const model = defineModel<string | null>();
 const emit = defineEmits(['update']);
 const props = defineProps<{
   label?: string;
+  help?: string;
 }>();
 
 const inputId = useId();
@@ -38,23 +40,31 @@ const onKeydown = (e: KeyboardEvent) => {
 </script>
 
 <template>
-  <FloatLabel variant="on">
-    <InputText
-      v-model="innerText"
-      v-bind="$attrs"
-      class="w-full h-full"
-      :id="inputId"
-      autocomplete="off"
-      :pt="{
-        root: () => {
-          return {
-            style: 'background-color: var(--p-inputtext-background);',
-          };
-        },
-      }"
-      @blur="save"
-      @keydown.stop="onKeydown"
-    />
-    <label :for="inputId">{{ props.label || '' }}</label>
-  </FloatLabel>
+  <div class="flex flex-col gap-1">
+    <FloatLabel variant="on">
+      <InputText
+        v-model="innerText"
+        v-bind="$attrs"
+        class="w-full h-full"
+        :id="inputId"
+        autocomplete="off"
+        :pt="{
+          root: () => {
+            return {
+              style: 'background-color: var(--p-inputtext-background);',
+            };
+          },
+        }"
+        @blur="save"
+        @keydown.stop="onKeydown"
+      />
+      <label :for="inputId">{{ props.label || '' }}</label>
+    </FloatLabel>
+    <Message
+      v-if="props.help != null"
+      size="small"
+      severity="secondary"
+      variant="simple"
+      >{{ props.help }}</Message>
+  </div>
 </template>
