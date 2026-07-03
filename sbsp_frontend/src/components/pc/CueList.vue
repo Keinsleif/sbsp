1bd226c08c4259bc718c1734b7187acc25ee527f
Keinsleif <mountaindull@gmail.com>
2026-07-03 15:04:42 +0900

    fix: scrollbar behavier

--- sbsp_frontend/src/components/pc/CueList.vue
+++ sbsp_frontend/src/components/pc/CueList.vue
@@ -278,7 +278,7 @@ const click = (event: MouseEvent, index: number) => {
 
 <template>
   <div
-    class="h-full scroll-pt-8 overflow-auto border border-(--p-form-field-border-color)"
+    class="h-full scroll-pt-8 overflow-scroll border border-(--p-form-field-border-color)"
     :class="$style['cuelist-wrapper']"
     tabindex="-1"
     @copy="copyHandler"
