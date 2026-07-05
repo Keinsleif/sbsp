c8f6de95a265b74709b9ed02c04ad43da18f9a6e
Keinsleif <mountaindull@gmail.com>
2026-07-05 16:20:20 +0900

    fix: rgb usage

--- sbsp_frontend/src/ConnectView.vue
+++ sbsp_frontend/src/ConnectView.vue
@@ -173,7 +173,7 @@ onUnmounted(() => {
       <div
         v-if="overlay"
         class="fixed inset-0 z-1000 h-full w-full flex items-center justify-center"
-        style="background-color: rgb(0, 0, 0, 0.5)"
+        style="background-color: rgb(0 0 0 / 0.5)"
       >
         <progress-spinner-wrapper
           color="primary.500"
