96f05c83e95a94cc34671c086542db7bb2527b59
Keinsleif <mountaindull@gmail.com>
2026-09-21 18:26:52 +0900

    fix: display classname and threshold of level meter

--- sbsp_frontend/src/components/display/LevelMeter.vue
+++ sbsp_frontend/src/components/display/LevelMeter.vue
@@ -54,13 +54,13 @@ const decayLoop = (timestamp: number) => {
     levels.right = Math.max(-60, levels.right - DECAY_PER_SEC * deltaTime);
   }
 
-  if (levels.left > 0) {
+  if (levels.left >= 0) {
     clipping.value.left = true;
     stopLeftClipReset();
     startLeftClipReset();
   }
 
-  if (levels.right > 0) {
+  if (levels.right >= 0) {
     clipping.value.right = true;
     stopRightClipReset();
     startRightClipReset();
@@ -102,7 +102,7 @@ onUnmounted(() => {
       >
         <div
           class="relative border border-(--p-form-field-border-color)"
-          :class="clipping.left ? 'bg-red' : 'bg-surface'"
+          :class="clipping.left ? 'bg-red-500' : ''"
           style="box-sizing: content-box; top: -2px"
           :style="{ width: props.width, height: props.width }"
         />
@@ -179,7 +179,7 @@ onUnmounted(() => {
       >
         <div
           class="relative border border-(--p-form-field-border-color)"
-          :class="clipping.right ? 'bg-red' : 'bg-surface'"
+          :class="clipping.right ? 'bg-red-500' : ''"
           style="box-sizing: content-box; top: -2px"
           :style="{ width: props.width, height: props.width }"
         />
